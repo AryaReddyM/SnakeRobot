eaac658df90eada80a0d38e66ea6235d2a401669
AryaReddyM <areddy7709@gmail.com>
2026-07-07 20:19:17 -0400

    Testing Servos

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,11 +1,29 @@
 #include <Arduino.h>
+#include <Bluepad32.h>
+#include <SCServo.h>
+
+SMS_STS servos;
+
+int degreesToSteps(float degrees) {
+    return (round(degrees * 4096.0f / 360.0f));
+}
 
 void setup() {
     Serial.begin(115200);
+    Serial2.begin(1000000);
+
+    delay(500);
+    servos.pSerial = &Serial2;
 
     Serial.println("Initial Print");
+
+    servos.EnableTorque(1, 1);
 }
 
 void loop() {
+    servos.WritePosEx(1, degreesToSteps(25), 1500, 50);
+    delay(3000);
 
+    servos.WritePosEx(1, degreesToSteps(0), 1500, 50);
+    delay(3000);
 }
