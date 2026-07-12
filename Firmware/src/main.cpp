f2fa884e59c15319e9523e2e01f7defc3b0174c4
AryaReddyM <areddy7709@gmail.com>
2026-07-11 21:09:52 -0400

    Test Snake Chain w/ Two Servos

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,29 +1,37 @@
 #include <Arduino.h>
-#include <Bluepad32.h>
 #include <SCServo.h>
 
 SMS_STS servos;
 
 int degreesToSteps(float degrees) {
-    return (round(degrees * 4096.0f / 360.0f));
+    return 2048 + (int)(degrees * 4096.0f / 360.0f);
 }
 
 void setup() {
     Serial.begin(115200);
-    Serial2.begin(1000000);
-
+    Serial2.begin(1000000, SERIAL_8N1, 22, 21);
     delay(500);
     servos.pSerial = &Serial2;
-
-    Serial.println("Initial Print");
-
-    servos.EnableTorque(1, 1);
+    Serial.println("Servo test starting...");
 }
 
 void loop() {
-    servos.WritePosEx(1, degreesToSteps(25), 1500, 50);
-    delay(3000);
+    float amplitude = 0.5;
+    float frequency = 0.8;
+    float phaseOffset = PI;
 
-    servos.WritePosEx(1, degreesToSteps(0), 1500, 50);
-    delay(3000);
+    float t = millis() / 1000.0;
+
+    for (int i = 0; i < 2; i++) {
+        float angle = amplitude * sin(2 * PI * frequency * t + i * phaseOffset);
+        float degrees = angle * 180.0 / 3.14159;
+        int steps = degreesToSteps(degrees);
+        servos.WritePosEx(i, steps, 0, 0);
+    }
+
+    delay(20);
+
+    int pos = servos.ReadPos(1);
+    Serial.print("Servo 1 position: ");
+    Serial.println(pos);
 }
