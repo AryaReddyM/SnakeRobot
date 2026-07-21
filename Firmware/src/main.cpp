7e25930f951ee3ca36b3bcae821d1ae85466d2c5
AryaReddyM <areddy7709@gmail.com>
2026-07-21 13:54:46 -0400

    Add Controller Input

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,10 +1,31 @@
 #include <Arduino.h>
 #include <SCServo.h>
+#include <Bluepad32.h>
+
+#define amplitude 0.5
+#define frequency 0.8
+#define phaseOffset PI
+#define segments 2
 
 SMS_STS servos;
+GamepadPtr gamepad;
 
-int degreesToSteps(float degrees) {
+int DegreesToSteps(float degrees) {
     return 2048 + (int)(degrees * 4096.0f / 360.0f);
+}
+
+float mapFloat(float x, float inMin, float inMax, float outMin, float outMax) {
+    return (x - inMin) / (inMax - inMin) * (outMax - outMin) + outMin;
+}
+
+void OnConnectedGamepad(GamepadPtr gp) {
+  Serial.println("Gamepad connected");
+  gamepad = gp;
+}
+
+void OnDisconnectedGamepad(GamepadPtr gp) {
+  Serial.println("Gamepad disconnected");
+  gamepad = nullptr;
 }
 
 void setup() {
@@ -12,26 +33,34 @@ void setup() {
     Serial2.begin(1000000, SERIAL_8N1, 22, 21);
     delay(500);
     servos.pSerial = &Serial2;
-    Serial.println("Servo test starting...");
+    
+    BP32.setup(&OnConnectedGamepad, &OnDisconnectedGamepad);
 }
 
 void loop() {
-    float amplitude = 0.5;
-    float frequency = 0.8;
-    float phaseOffset = PI;
+    BP32.update();
 
-    float t = millis() / 1000.0;
+    if (gamepad && gamepad->isConnected()) {
+        int leftY = -(gamepad->axisY());
+        int leftX = gamepad->axisX();
+        
+        float turn = mapFloat(leftX, -512, 512, -0.2f, 0.2f);
 
-    for (int i = 0; i < 2; i++) {
-        float angle = amplitude * sin(2 * PI * frequency * t + i * phaseOffset);
-        float degrees = angle * 180.0 / 3.14159;
-        int steps = degreesToSteps(degrees);
-        servos.WritePosEx(i, steps, 0, 0);
+        if (leftY > 150 || leftX > 50 || leftX < -50) {
+            float t = millis() / 1000.0;
+
+            for (int i = 0; i < segments; i++) {
+                float angle = amplitude * sin(2 * PI * frequency * t + i * phaseOffset) + turn;
+                float degrees = angle * 180.0 / 3.14159;
+                int steps = DegreesToSteps(degrees);
+                servos.WritePosEx(i, steps, 0, 0);
+            }
+        }
+
+        Serial.print(leftX);
+        Serial.print(", ");
+        Serial.println(turn);
     }
 
     delay(20);
-
-    int pos = servos.ReadPos(1);
-    Serial.print("Servo 1 position: ");
-    Serial.println(pos);
 }
