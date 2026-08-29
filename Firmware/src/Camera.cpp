a75ad5ffe5c2420f6589858e2fd0f39840c3091d
AryaReddyM <areddy7709@gmail.com>
2026-08-29 19:47:42 -0400

    Flipped The Video Playback Upside-down because I Mounted the ESP32 Camera Module Upside-down

--- Firmware/src/Camera.cpp
+++ Firmware/src/Camera.cpp
@@ -6,53 +6,44 @@
 const char* ssid = "Fios-2c3PM";
 const char* password = "ink565jag85coop";
 
-#define PWDN_PIN     32
-#define RST_PIN      -1
-#define XCLK_PIN      0
-#define SIOD_PIN     26
-#define SIOC_PIN     27
-#define Y9_PIN       35
-#define Y8_PIN       34
-#define Y7_PIN       39
-#define Y6_PIN       36
-#define Y5_PIN       21
-#define Y4_PIN       19
-#define Y3_PIN       18
-#define Y2_PIN        5
-#define VSYNC_PIN    25
-#define HREF_PIN     23
-#define PCLK_PIN     22
+#define PWDN_PIN 32
+#define RST_PIN -1
+#define XCLK_PIN 0
+#define SIOD_PIN 26
+#define SIOC_PIN 27
+#define Y9_PIN 35
+#define Y8_PIN 34
+#define Y7_PIN 39
+#define Y6_PIN 36
+#define Y5_PIN 21
+#define Y4_PIN 19
+#define Y3_PIN 18
+#define Y2_PIN 5
+#define VSYNC_PIN 25
+#define HREF_PIN 23
+#define PCLK_PIN 22
 
 httpd_handle_t server = NULL;
 
 #define BOUNDARY_STR "123456789000000000000987654321"
-
-static const char* streamType =
-    "multipart/x-mixed-replace;boundary=" BOUNDARY_STR;
-
-static const char* boundary =
-    "\r\n--" BOUNDARY_STR "\r\n";
-
-static const char* headerFmt =
-    "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+static const char* streamType = "multipart/x-mixed-replace;boundary=" BOUNDARY_STR;
+static const char* boundary = "\r\n--" BOUNDARY_STR "\r\n";
+static const char* headerFmt = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
 static esp_err_t streamHandler(httpd_req_t* req) {
   camera_fb_t* frame = NULL;
   esp_err_t err = ESP_OK;
-
   size_t dataLen = 0;
   uint8_t* data = NULL;
   char header[64];
 
   err = httpd_resp_set_type(req, streamType);
-
   if (err != ESP_OK) {
     return err;
   }
 
   while (true) {
     frame = esp_camera_fb_get();
-
     if (!frame) {
       Serial.println("Cam fail");
       err = ESP_FAIL;
@@ -63,32 +54,22 @@ static esp_err_t streamHandler(httpd_req_t* req) {
 
     if (err == ESP_OK) {
       err = httpd_resp_send_chunk(
-        req,
-        boundary,
-        strlen(boundary)
+        req, boundary, strlen(boundary)
       );
     }
 
     if (err == ESP_OK) {
       size_t headerLen = snprintf(
-        header,
-        sizeof(header),
-        headerFmt,
-        dataLen
+        header, sizeof(header), headerFmt, dataLen
       );
-
       err = httpd_resp_send_chunk(
-        req,
-        header,
-        headerLen
+        req, header, headerLen
       );
     }
 
     if (err == ESP_OK) {
       err = httpd_resp_send_chunk(
-        req,
-        (const char*)data,
-        dataLen
+        req, (const char*)data, dataLen
       );
     }
 
@@ -131,10 +112,8 @@ void setup() {
   Serial.println();
 
   camera_config_t camConfig;
-
   camConfig.ledc_channel = LEDC_CHANNEL_0;
   camConfig.ledc_timer = LEDC_TIMER_0;
-
   camConfig.pin_d0 = Y2_PIN;
   camConfig.pin_d1 = Y3_PIN;
   camConfig.pin_d2 = Y4_PIN;
@@ -143,7 +122,6 @@ void setup() {
   camConfig.pin_d5 = Y7_PIN;
   camConfig.pin_d6 = Y8_PIN;
   camConfig.pin_d7 = Y9_PIN;
-
   camConfig.pin_xclk = XCLK_PIN;
   camConfig.pin_pclk = PCLK_PIN;
   camConfig.pin_vsync = VSYNC_PIN;
@@ -152,28 +130,29 @@ void setup() {
   camConfig.pin_sccb_scl = SIOC_PIN;
   camConfig.pin_pwdn = PWDN_PIN;
   camConfig.pin_reset = RST_PIN;
-
   camConfig.xclk_freq_hz = 20000000;
   camConfig.pixel_format = PIXFORMAT_JPEG;
-
   camConfig.frame_size = FRAMESIZE_VGA;
   camConfig.jpeg_quality = 12;
   camConfig.fb_count = 2;
 
   esp_err_t initErr = esp_camera_init(&camConfig);
-
   if (initErr != ESP_OK) {
     Serial.printf("Init error 0x%x", initErr);
     return;
   }
 
-  WiFi.begin(ssid, password);
+  sensor_t * s = esp_camera_sensor_get();
+  if (s != NULL) {
+    s->set_vflip(s, 1);
+    s->set_hmirror(s, 1);
+  }
 
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
   Serial.println("");
   Serial.println("Connected");
 
